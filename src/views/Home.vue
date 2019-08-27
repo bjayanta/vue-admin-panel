3b6a200b7e5709fc2afcc339f118eee70eb1858f
Jayanta Biswas <bjayanta.me@gmail.com>
2019-08-27 19:11:50 +0600

    First commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Landing />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Landing from '@/components/Landing';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'home',
+    components: {
+        Landing,
+    },
+};
 </script>
---
+++ src/views/pos/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-row align="center" justify="center" style="height: 100%;">
+                <v-col sm="auto">
+                    <v-card>
+                        <v-card-title>I'm a title</v-card-title>
+                        <v-card-text>I'm card text</v-card-text>
+
+                        <v-card-actions>
+                            <v-btn text>Click</v-btn>
+                        </v-card-actions>
+                    </v-card>
+
+                    <v-btn color="warning" dark width="200" height="200" to="/pos">
+                        <strong>POS</strong>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+};
+</script>
